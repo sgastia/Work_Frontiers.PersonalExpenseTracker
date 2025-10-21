b2373da7b8c441bdf44c3ba627dac06c9b863118
Sebastian <sebastian.gastiazoro@hotmail.com>
2025-10-21 20:46:14 +0200

    Add project files.

---
+++ Frontiers.PersonalExpenseTracker.UI.Server/Program.cs
@@ -0,0 +1,32 @@
+namespace Frontiers.PersonalExpenseTracker.UI.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.MapStaticAssets();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
